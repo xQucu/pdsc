47076ef4126e1d49c5edbf4cdf79723c1f88b232
Adrian Kucyk <kucykadrian2004@gmail.com>
2025-04-09 23:20:14 +0200

    initial commit for bsearch

---
+++ bsearch/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void *bsearch(const void *key, const void *base, size_t num, size_t size, int (*compar)(const void *, const void *));
+
+int main()
+{
+    printf("Hello world");
+    return 0;
+}
+void *bsearch(const void *key, const void *base, size_t num, size_t size, int (*compar)(const void *, const void *))
+{
+
+    return;
+}
