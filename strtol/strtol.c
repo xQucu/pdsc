a27f815f103fd3bd6770321808f96944ec9d66a0
Adrian Kucyk <kucykadrian2004@gmail.com>
2025-04-27 19:40:33 +0200

    refactor: extract functions checkSign and checkBasePrefix and UpdateResult

--- strtol/strtol.c
+++ strtol/strtol.c
@@ -50,6 +50,131 @@ void checkSpaces(const char *nPtr, int *counter, char *c)
 	}
 }
 
+bool checkSign(const char *nPtr, int *counter, char *c, int *signHelper, char **endPtr, long *result)
+{
+	if (*c == '-' && *(nPtr + *counter + 1) != '\0')
+	{
+		*signHelper = -1;
+		(*counter)++;
+		*c = *(nPtr + *counter);
+	}
+	else if (*c == '+' && *(nPtr + *counter + 1) != '\0')
+	{
+		*signHelper = 1;
+		(*counter)++;
+		*c = *(nPtr + *counter);
+	}
+	else if (*c == '+' || *c == '-')
+	{
+		if (endPtr != NULL)
+		{
+			*endPtr = (char *)nPtr;
+		}
+		*result = 0;
+		return false;
+	}
+	return true;
+}
+
+bool checkBasePrefix(const char *nPtr, int *counter, char *c, int *base, char **endPtr, long *result)
+{
+	if (*base == 0)
+	{
+		if (*c == '0' && *(nPtr + *counter + 1) == 'x')
+		{
+			*base = 16;
+			*counter += 2;
+			*c = *(nPtr + *counter);
+			if (endPtr != NULL && *c == '\0')
+			{
+				*endPtr = (char *)(nPtr + *counter - 1);
+				return false;
+			}
+			if (toInt(*c) == -1 || toInt(*c) >= *base || *c == '\0')
+			{
+				if (endPtr != NULL)
+				{
+					*endPtr = (char *)(nPtr + *counter - 1);
+				}
+				return false;
+			}
+		}
+		else if (*c == '0')
+		{
+			(*counter)++;
+			*c = *(nPtr + *counter);
+			*base = 8;
+
+			if (endPtr != NULL && *c == '\0')
+			{
+				*endPtr = (char *)(nPtr + *counter);
+				return false;
+			}
+			if (toInt(*c) == -1 || toInt(*c) >= *base)
+			{
+				if (endPtr != NULL)
+				{
+					*endPtr = (char *)(nPtr + *counter);
+				}
+				return false;
+			}
+		}
+		else
+		{
+			*base = 10;
+		}
+	}
+	else if (*base == 8)
+	{
+		if (*c == '0')
+		{
+			(*counter)++;
+			*c = *(nPtr + *counter);
+		}
+		if (toInt(*c) == -1 || toInt(*c) >= *base)
+		{
+			if (endPtr != NULL)
+			{
+				*endPtr = (char *)(nPtr + *counter);
+			}
+			return false;
+		}
+	}
+	else if (*base == 16)
+	{
+		if (*c == '0' && *(nPtr + *counter + 1) == 'x')
+		{
+			*counter += 2;
+			*c = *(nPtr + *counter);
+		}
+
+		if (toInt(*c) == -1 || toInt(*c) >= *base || *c == '\0')
+		{
+			if (endPtr != NULL)
+			{
+				*endPtr = (char *)(nPtr + *counter - 1);
+			}
+			return false;
+		}
+	}
+
+	return true;
+}
+
+long updateResult(long result, char c, int base, int signHelper)
+{
+	if (result >= 0 && (LONG_MAX - toInt(c)) / base < result)
+	{
+		errno = ERANGE;
+		return LONG_MAX;
+	}
+	if (result < 0 && (LONG_MIN + toInt(c)) / base > result)
+	{
+		errno = ERANGE;
+		return LONG_MIN;
+	}
+	return result * base + toInt(c) * signHelper;
+}
 
 long strtol(const char *nPtr, char **endPtr, int base)
 {
@@ -67,6 +192,7 @@ long strtol(const char *nPtr, char **endPtr, int base)
 
 	checkSpaces(nPtr, &counter, &c);
 
+	// check for end of string
 	if (c == '\0')
 	{
 		if (endPtr != NULL)
@@ -76,103 +202,16 @@ long strtol(const char *nPtr, char **endPtr, int base)
 		return result;
 	}
 
-	// check sign
-	if (c == '-' && *(nPtr + counter + 1) != '\0')
+	ok = checkSign(nPtr, &counter, &c, &signHelper, endPtr, &result);
+	if (!ok)
 	{
-		signHelper = -1;
-		counter++;
-		c = *(nPtr + counter);
-	}
-	else if (c == '+' && *(nPtr + counter + 1) != '\0')
-	{
-		signHelper = 1;
-		counter++;
-		c = *(nPtr + counter);
-	}
-	else if (c == '+' || c == '-')
-	{
-		if (endPtr != NULL)
-		{
-			*endPtr = (char *)nPtr;
-		}
 		return result;
 	}
 
-	// check prefix
-	if (base == 0)
+	ok = checkBasePrefix(nPtr, &counter, &c, &base, endPtr, &result);
+	if (!ok)
 	{
-		if (c == '0' && *(nPtr + counter + 1) == 'x')
-		{
-			base = 16;
-			counter += 2;
-			c = *(nPtr + counter);
-			if (endPtr != NULL && c == '\0')
-			{
-				*endPtr = (char *)(nPtr + counter - 1);
-				return result;
-			}
-			if (toInt(c) == -1 || toInt(c) >= base || c == '\0')
-			{
-				*endPtr = (char *)(nPtr + counter - 1);
-				return result;
-			}
-		}
-		else if (c == '0')
-		{
-			counter++;
-			c = *(nPtr + counter);
-			base = 8;
-
-			if (endPtr != NULL && c == '\0')
-			{
-				*endPtr = (char *)(nPtr + counter);
-				return result;
-			}
-			if (toInt(c) == -1 || toInt(c) >= base)
-			{
-				*endPtr = (char *)(nPtr + counter);
-				return result;
-			}
-		}
-		else
-		{
-			base = 10;
-		}
-	}
-	else if (base == 8)
-	{
-		if (c == '0')
-		{
-			counter++;
-			c = *(nPtr + counter);
-			base = 8;
-		}
-		if (toInt(c) == -1 || toInt(c) >= base)
-		{
-			if (endPtr != NULL)
-			{
-				*endPtr = (char *)(nPtr + counter);
-			}
-			return result;
-		}
-	}
-	else if (base == 16)
-	{
-		if (c == '0' && *(nPtr + counter + 1) == 'x')
-		{
-			counter += 2;
-			c = *(nPtr + counter);
-			base = 16;
-		}
-
-		if (toInt(c) == -1 || toInt(c) >= base || c == '\0')
-		{
-			if (endPtr != NULL)
-			{
-				*endPtr = (char *)(nPtr + counter - 1);
-			}
-			return result;
-		}
+		return result;
 	}
 
 	if (toInt(c) == -1 || toInt(c) >= base || c == '\0')
@@ -194,20 +233,7 @@ long strtol(const char *nPtr, char **endPtr, int base)
 			}
 			return result;
 		}
-		if (result >= 0 && (LONG_MAX - toInt(c)) / base < result)
-		{
-			errno = ERANGE;
-			result = LONG_MAX;
-		}
-		else if (result < 0 && (LONG_MIN + toInt(c)) / base > result)
-		{
-			errno = ERANGE;
-			result = LONG_MIN;
-		}
-		else
-		{
-			result = result * base + toInt(c) * signHelper;
-		}
+		result = updateResult(result, c, base, signHelper);
 
 		counter++;
 		c = *(nPtr + counter);
